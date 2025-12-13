fb0fdf523a6f349b49ca82b7fb06237baebf26ce
JC Guerrero <JCystems@gMail.com>
2025-12-13 13:30:06 -0600

    fix(`workflows/test_actions`): added `checkout` (#4)

---
+++ assets/dotnet/SemanticKernelSampler/Program.cs
@@ -0,0 +1,8 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="JCystems">
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ assets/dotnet/console/Program.cs
@@ -0,0 +1,4 @@
+﻿// See LICENSE file in the project root for full license information.
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
