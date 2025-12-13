e2c565487aab44faa8fc5534c9d4a33296f8673f
JCGuerrero <JCystems@gMail.com>
2025-12-13 13:14:42 -0600

    sc

--- assets/dotnet/slnx/Program.cs
+++ assets/dotnet/slnx/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="JCystems">
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
