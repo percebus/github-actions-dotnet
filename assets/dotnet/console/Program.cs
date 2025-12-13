a8244beb622fda6db99a0b27c7c5f877a79f8c18
JCGuerrero <JCystems@gMail.com>
2025-12-13 13:05:02 -0600

    L

--- assets/dotnet/console/Program.cs
+++ assets/dotnet/console/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See LICENSE file in the project root for full license information.
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
